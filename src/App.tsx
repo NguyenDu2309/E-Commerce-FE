c57d140ae5e8849ee82cea8c0669dcea1fb6d3bf
Nguyen Du <nguyendu2004.anhuu@gmail.com>
2025-11-20 23:37:26 +0700

    useState example

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
+
 function App() {
-  return (
-     <div>
-        <h1>Hello World</h1>
-     </div>
-  );
+   const [products, setProducts] = useState([
+      { name: 'Laptop', price: 999 },
+      { name: 'Smartphone', price: 699 },
+      { name: 'Tablet', price: 499 },
+   ]);
+   function addProduct(name: string, price: number) {
+      setProducts(prevState => [...prevState, { name: "Monitor" + prevState.length + 1, price: (prevState.length * 100) + 100 }]);
+   }
+   return (
+      <div>
+         <h1 style={{ color: 'red' }}>Re-Store</h1>
+         <ul>
+            {products.map((item, index) => (
+               <li key={index}>
+                  {item.name}: ${item.price}
+               </li>
+            ))}
+         </ul>
+         <button onClick={() => addProduct("Monitor", 399)}>Add Monitor</button>
+      </div>
+   );
 }
 
 export default App;
