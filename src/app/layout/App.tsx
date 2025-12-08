b2d62169c1a8745ffe4ac8b653f2523d32abefe6
Nguyen Du <nguyendu2004.anhuu@gmail.com>
2025-12-08 22:40:55 +0700

    add darkmode

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -5,9 +5,22 @@ import Catalog from "../../features/catalog/Catalog";
 import Header from "./Header";
 import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material";
 
 
 function App() {
+
+   const [darkMode, setDarkMode] = useState(false);
+   const palleteType = darkMode ? 'dark' : 'light';
+   const theme = createTheme({
+      palette: {
+         mode: palleteType,
+         background: {
+            default: palleteType === 'light' ? '#eaeaea' : '#121212'
+         }
+      }
+   });
+
    const [products, setProducts] = useState<Product[]>([]);
 
    useEffect(() => {
@@ -17,28 +30,33 @@ function App() {
          .catch(error => console.error('Error fetching products:', error));
    }, []);
 
-   function addProduct() 
-   {
-      setProducts(prevState => [...prevState, 
-         { 
-            id: prevState.length + 1,
-            name: "Monitor" + prevState.length + 1, 
-            price: (prevState.length * 100) + 100,
-            description: "A high-resolution monitor",
-            pictureUrl: "images/hero1.jpg",
-            type: "Electronics",
-            brand: "Generic",
-            quantityInStock: 50
-         }]);
+   function addProduct() {
+      setProducts(prevState => [...prevState,
+      {
+         id: prevState.length + 1,
+         name: "Monitor" + prevState.length + 1,
+         price: (prevState.length * 100) + 100,
+         description: "A high-resolution monitor",
+         pictureUrl: "images/hero1.jpg",
+         type: "Electronics",
+         brand: "Generic",
+         quantityInStock: 50
+      }]);
    }
+
+   function handleThemeChange() {
+      setDarkMode(!darkMode);
+   }
+
    return (
-      <>
-         <CssBaseline/>
-         <Header/>
+      <ThemeProvider theme={theme}>
+         <CssBaseline />
+         <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
          <Container>
-            <Catalog products = {products} addProduct={addProduct} />
+            <Catalog products={products} addProduct={addProduct} />
          </Container>
-      </>
+      </ThemeProvider>
+
    );
 }
 
