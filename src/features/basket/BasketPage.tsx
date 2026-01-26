cc6fc051276842015ada05f0f3915eb106a8eaac
Nguyen Du <nguyendu2004.anhuu@gmail.com>
2026-01-26 18:27:56 +0700

    add StoreContext

--- src/features/basket/BasketPage.tsx
+++ src/features/basket/BasketPage.tsx
@@ -2,9 +2,20 @@ import { useEffect, useState } from "react";
 import { Basket } from "../../app/models/basket";
 import agent from "../../app/api/agent";
 import LoadingComponent from "../../app/layout/LoadingComponent";
-import { Typography } from "@mui/material";
+import {
+    IconButton,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Typography
+} from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
 
-export default function BasketPage(){
+export default function BasketPage() {
     const [loading, setLoading] = useState(true);
     const [basket, setBasket] = useState<Basket | null>(null);
 
@@ -15,11 +26,50 @@ export default function BasketPage(){
             .finally(() => setLoading(false));
     }, []);
 
-    if (loading) return <LoadingComponent message="Loading basket..."/>
+    if (loading) return <LoadingComponent message="Loading basket..." />;
 
-    if (!basket) return <Typography>Your basket is empty</Typography>
+    if (!basket || basket.items.length === 0)
+        return <Typography>Your basket is empty</Typography>;
 
     return (
-        <Typography>{basket.items.length} items in your basket</Typography>
-    )
+        <TableContainer component={Paper}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Product</TableCell>
+                        <TableCell align="right">Price</TableCell>
+                        <TableCell align="center">Quantity</TableCell>
+                        <TableCell align="right">Subtotal</TableCell>
+                        <TableCell /> {/* cột actions */}
+                    </TableRow>
+                </TableHead>
+
+                <TableBody>
+                    {basket.items.map(item => (
+                        <TableRow key={item.productId}>
+                            <TableCell>{item.name}</TableCell>
+
+                            <TableCell align="right">
+                                ${(item.price / 100).toFixed(2)}
+                            </TableCell>
+
+                            <TableCell align="center">
+                                {item.quantity}
+                            </TableCell>
+
+                            <TableCell align="right">
+                                ${((item.price / 100) * item.quantity).toFixed(2)}
+                            </TableCell>
+
+                            <TableCell align="right">
+                                <IconButton color="error">
+                                    <DeleteIcon />
+                                </IconButton>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
+import { StoreProvider } from './app/context/StoreContext';
 
 export const history = createBrowserHistory();
 
@@ -12,7 +13,9 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <Router history={history}>
-      <App />
+      <StoreProvider>
+        <App />
+      </StoreProvider>
     </Router>
   </React.StrictMode>
 );
